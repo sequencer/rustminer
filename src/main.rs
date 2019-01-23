631a57d8755c701363b786a8ddf9f18e6354c3c5
nanpuyue <nanpuyue@gmail.com>
2019-01-23 14:01:53 +0800

    communicate with asic

--- src/main.rs
+++ src/main.rs
@@ -3,13 +3,14 @@
 
 use serde_json::json;
 use tokio::prelude::*;
-use tokio::runtime::current_thread::Runtime;
 
 mod util;
 pub mod work;
 pub mod stratum;
 
+use self::work::*;
 use self::stratum::*;
+use self::util::serial::serial_framed;
 
 fn main() {
     let mut pool = Pool::new("cn.ss.btc.com:1800");
@@ -34,10 +35,25 @@ fn main() {
     println!("4,{:?}", ret);
 
     let ws = WorkStream(pool.works.clone());
+    let xnonce = pool.xnonce.clone();
     let task = ws
-        .for_each(|w| {
-            println!("{:?}", w);
+        .for_each(move |w| {
+            let xnonce = xnonce.lock().unwrap();
+            let (sink, stream) = serial_framed("/dev/ttyUSB0").split();
+            let send_to_asic = SubWorkMaker::new(w, &xnonce)
+                .forward(sink)
+                .then(|_| Ok(()));
+            tokio::spawn(send_to_asic);
+
+            let receive_from_asic = stream
+                .for_each(|s| {
+                    println!("received {} bytes: {:?}", s.len(), s);
+                    Ok(())
+                }).map_err(|e| eprintln!("{}", e));
+            tokio::spawn(receive_from_asic);
+
             Ok(())
         });
+
     tokio::run(task);
 }
