21096da8de4720634b2c755f63d0854d4e5d3b56
nanpuyue <nanpuyue@gmail.com>
2019-04-01 12:31:08 +0800

    use `debug_assert_eq!` instead of `assert_eq!`

--- src/util/fpga.rs
+++ src/util/fpga.rs
@@ -90,13 +90,13 @@ impl Writer {
     pub fn writer_subwork2(&mut self, sw2: Subwork2) {
         self.mmap.write(0, sw2.version.to_be_bytes());
         self.mmap.write(4, sw2.vermask.to_be_bytes());
-        assert_eq!(sw2.prevhash.len(), 32);
+        debug_assert_eq!(sw2.prevhash.len(), 32);
         self.mmap.write(8, &sw2.prevhash);
-        assert_eq!(sw2.merkle_root.len(), 32);
+        debug_assert_eq!(sw2.merkle_root.len(), 32);
         self.mmap.write(40, &sw2.merkle_root);
-        assert_eq!(sw2.ntime.len(), 4);
+        debug_assert_eq!(sw2.ntime.len(), 4);
         self.mmap.write(72, &sw2.ntime);
-        assert_eq!(sw2.nbits.len(), 4);
+        debug_assert_eq!(sw2.nbits.len(), 4);
         self.mmap.write(76, &sw2.nbits);
 
         self.subworks.push_front(sw2);
